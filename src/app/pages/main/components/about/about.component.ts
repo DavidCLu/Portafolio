c73efd42d6b2206163e79f42ce492ceae3418a0f
dx <pyke.it.proyect@gmail.com>
2023-02-04 02:44:15 +0100

    ramaV1

--- src/app/pages/main/components/about/about.component.ts
+++ src/app/pages/main/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  scroll:number=1;
+  scroll1:number=1;
+  moveLeft:boolean=false;
+  moveRight:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+
   }
+  @HostListener('window:scroll', ['$event']) 
+  onScrollEvent($event: any) {
+    // console.log(window.pageYOffset);
+    this.scroll = window.pageYOffset;
+    
+    if(this.scroll > 550 ){
+      this.moveRight= false;
+      this.moveLeft = true;
+      //  this.transport = this.scroll - 1000
+     
+      // console.log(this.transport)
+    }
+    if(this.scroll < 700  ){
+      this.moveLeft = false
+      this.moveRight = true
+    console.log(this.moveLeft)} 
+
+    
+   
+}
 
 }
+
+
+
