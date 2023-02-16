aa48d36542fb031f405fb71c92509e359dbfdb46
dx <pyke.it.proyect@gmail.com>
2023-02-16 23:42:23 +0100

    edit img resolution

--- src/app/pages/main/components/cube-in/cube.component.ts
+++ src/app/pages/main/components/cube-in/cube.component.ts
@@ -12,6 +12,7 @@ export class CubeComponent implements OnInit {
 @Input('boxSide') faceSideIdValue = "";
 
 @Output()  closeComponent = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
